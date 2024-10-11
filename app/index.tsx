d0d34a48b23b3ee99127ba69a8bfa7ca96296651
assadi-dev <assadi.halifa@gmail.com>
2024-10-11 12:45:17 +0200

    "Updated SplashScreen component to use reanimated's useSharedValue, useAnimatedStyle, and withTiming for animation, and removed infoStorage call"

--- app/index.tsx
+++ app/index.tsx
@@ -3,23 +3,31 @@ import React from 'react'
 import {  router } from 'expo-router'
 import { infoStorage } from '@/lib/fileSystem'
 import LottieView from 'lottie-react-native';
-import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
+import Animated, { FadeIn, FadeOut, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 const SplashScreen = () => {
 
   const animation = React.useRef<LottieView>(null);
-
+  const scale = useSharedValue<number>(0);
+  const animationStyles = useAnimatedStyle(() => ({
+    opacity: scale.value,
+  }));
 
   React.useEffect(() => {
 
-    //infoStorage()
+     scale.value = withRepeat(
+      withTiming( scale.value + 1 , { duration: 500 }),
+      -1,
+      true
+    ); 
+
     const timeID = setTimeout(() => {
     router.push('/home')
     }, 1000)
      
     return () => {
      clearTimeout(timeID)
-    }
+    } 
 
 },[])
   
@@ -39,7 +47,9 @@ const SplashScreen = () => {
 
         source={require('../assets/lotties/Animation-1728639201392.json')}
       />
-      <Animated.Text className="text-white text-xl mt-3"  entering={FadeIn.delay(150)} exiting={FadeOut}  >Veuillez patientez</Animated.Text>
+      <Animated.Text className="text-white text-xl mt-3"  style={
+      [  animationStyles]
+      }  >Veuillez patientez</Animated.Text>
      
     </View>
   )
